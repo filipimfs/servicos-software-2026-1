f6af8a0831546f433bb676f61d906b9c7f9270ae
Marco Mazzei <marco.mazzei@maua.br>
2026-03-26 21:03:40 -0300

    Arquivo backend modificado

--- servicos_software_p/backend-json/app/main.py
+++ servicos_software_p/backend-json/app/main.py
@@ -1,32 +1,35 @@
 import os
 import shutil
 import whisper
-from fastapi import FastAPI, File, UploadFile
+from typing import Optional, Union
+from fastapi import FastAPI
+from fastapi import File, UploadFile
+
+import json
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# Carrega o modelo (o 'base' ou 'tiny' é rápido de baixar e ideal para uma aula de 4h)
 print("Carregando modelo de IA (Whisper)...")
 model = whisper.load_model("base")
 print("Modelo carregado!")
 
 
+@app.get("/")
+def diz_ola():
+    return {"Olá": "Mundo"}
+
+
 @app.post("/transcrever")
 async def transcrever_audio(file: UploadFile = File(...)):
-    # 1. Salva o arquivo temporariamente no container
     caminho_temp = f"temp_{file.filename}"
     with open(caminho_temp, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     try:
-        # 2. IA converte o áudio para texto.
-        # Forçamos o idioma para português para maior precisão e rapidez.
         resultado = model.transcribe(caminho_temp, language="pt")
         texto = resultado["text"].strip()
     finally:
-        # 3. Limpa o arquivo de áudio temporário para não lotar o container
         if os.path.exists(caminho_temp):
             os.remove(caminho_temp)
-
-    # Retorna o texto que servirá como prompt para o próximo passo do algoritmo
     return {"texto": texto}
